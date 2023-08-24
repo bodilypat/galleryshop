4b3121c8dc7ff312db725aaa4094377c2caa231c
bodilypat <139784676+bodilypat@users.noreply.github.com>
2023-08-24 09:42:06 -0700

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -1,16 +1,16 @@
 <?php
 session_start();
 error_reporting(0);
-include('includes/dbconnect.php');
+include('includes/dbconnection.php');
 
 if(isset($_POST['login']))
   {
     $adminuser=$_POST['username'];
     $password=md5($_POST['password']);
-    $query=mysqli_query($con,"select ID from tbladmin where  UserName='$adminuser' && Password='$password' ");
-    $ret=mysqli_fetch_array($query);
+    $qAdmin=mysqli_query($con,"SELECT ID FROM tbladmin WHERE  UserName='$adminuser' && Password='$password' ");
+    $result=mysqli_fetch_array($qAdmin);
     if($ret>0){
-      $_SESSION['agmsaid']=$ret['ID'];
+      $_SESSION['agmsaid']=$result['ID'];
       echo "<script type='text/javascript'> document.location ='dashboard.php'; </script>";
     }
     else{
@@ -23,51 +23,32 @@ if(isset($_POST['login']))
 <html lang="en">
 
 <head>
-  <title>Login| Art Gallery Management System</title>
-
-  <!-- Bootstrap CSS -->
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-  <!-- bootstrap theme -->
-  <link href="css/bootstrap-theme.css" rel="stylesheet">
-  <!--external css-->
-  <!-- font icon -->
-  <link href="css/elegant-icons-style.css" rel="stylesheet" />
-  <link href="css/font-awesome.css" rel="stylesheet" />
-  <!-- Custom styles -->
-  <link href="css/style.css" rel="stylesheet">
-  <link href="css/style-responsive.css" rel="stylesheet" />
-
+  <title>Login| Gallery Shop Management </title>  
+	  <link href="css/bootstrap.min.css" rel="stylesheet">
+	  <link href="css/bootstrap-theme.css" rel="stylesheet">
+	  <link href="css/elegant-icons-style.css" rel="stylesheet" />
+	  <link href="css/font-awesome.css" rel="stylesheet" />
+	  <link href="css/style.css" rel="stylesheet">
+	  <link href="css/style-responsive.css" rel="stylesheet" />
 </head>
-
 <body class="login-img3-body">
-
-  <div class="container">
-
-    <form class="login-form" action="" method="post">
-      
-      <div class="login-wrap">
-        <p class="login-img"><i class="icon_lock_alt"></i></p>
-        <div class="input-group">
-          <span class="input-group-addon"><i class="icon_profile"></i></span>
-          <input type="text" class="form-control" name="username" placeholder="Username" autofocus required="true">
-        </div>
-        <div class="input-group">
-          <span class="input-group-addon"><i class="icon_key_alt"></i></span>
-          <input type="password" class="form-control" name="password" placeholder="Password" required="true">
-        </div>
-        
-                
-                <lable><span class="pull-right"> <a href="forgot-password.php"> Forgot Password?</a></span>
-            </label>
-        <button class="btn btn-primary btn-lg btn-block" type="submit" name="login">Login</button>
-            <p style="margin-top:3%; font-weight:bold"><a href="../index.php" >Back to Home page</a></p>
-      </div>
-    </form>
-
-   
-  </div>
-
-
+	  <div class="container">
+			<form class="login-form" action="" method="post"> 
+				  <div class="login-wrap">
+						<p class="login-img"><i class="icon_lock_alt"></i></p>
+						<div class="input-group">
+							  <span class="input-group-addon"><i class="icon_profile"></i></span>
+							  <input type="text" class="form-control" name="username" placeholder="Username" autofocus required="true">
+						</div>
+						<div class="input-group">
+							<span class="input-group-addon"><i class="icon_key_alt"></i></span>
+							<input type="password" class="form-control" name="password" placeholder="Password" required="true">
+						</div>          
+							<lable><span class="pull-right"> <a href="forgot-password.php"> Forgot Password?</a></span> </label>       
+						<button class="btn btn-primary btn-lg btn-block" type="submit" name="login">Login</button>
+						<p style="margin-top:3%; font-weight:bold"><a href="../index.php" >Back to Home page</a></p>
+				  </div>
+			</form>  
+	  </div>
 </body>
-
 </html>

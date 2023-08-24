4e1be9357471006d3d15cba9a06e9725b3033554
bodilypat <139784676+bodilypat@users.noreply.github.com>
2023-08-24 09:44:25 -0700

    Update and rename manageArtist.php to handleArtist.php

--- admin/handleArtist.php
+++ admin/handleArtist.php
@@ -9,45 +9,29 @@
 
 	if(isset($_GET['delid']))
 	{
-		$rid=intval($_GET['delid']);
-		$sql=mysqli_query($con,"delete from tblartist where ID='$rid'");
+		$did=intval($_GET['delid']);
+		$delArt=mysqli_query($con,"delete from tblartist where ID='$did'");
 		echo "<script>alert('Data deleted');</script>"; 
-		echo "<script>window.location.href = 'manage-artist.php'</script>";     
+		echo "<script>window.location.href = 'handleArtist.php'</script>";     
 	}
   ?>
   
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-
-  <title>Manage Artist | Shop Gallery Management </title>
-
-  <!-- Bootstrap CSS -->
+  <title>Manage Artist | Gallery Shop Management </title>
   <link href="css/bootstrap.min.css" rel="stylesheet">
-  <!-- bootstrap theme -->
   <link href="css/bootstrap-theme.css" rel="stylesheet">
-  <!--external css-->
-  <!-- font icon -->
   <link href="css/elegant-icons-style.css" rel="stylesheet" />
   <link href="css/font-awesome.min.css" rel="stylesheet" />
-  <!-- Custom styles -->
   <link href="css/style.css" rel="stylesheet">
   <link href="css/style-responsive.css" rel="stylesheet" />
-
 </head>
-
 <body>
-  <!-- container section start -->
-  <section id="container" class="">
-    <!--header start-->
+
+<section id="container" class="">
     <?php include_once('includes/header.php');?>
-    <!--header end-->
-
-    <!--sidebar start-->
     <?php include_once('includes/sidebar.php');?>
-
-			<!--main content start-->
 			<section id="main-content">
 				  <section class="wrapper">
 						<div class="row">
@@ -60,7 +44,6 @@
 									</ol>
 							  </div>
 						</div>
-					<!-- page start-->
 					<div class="row">
 						  <div class="col-sm-12">
 								<section class="panel">
@@ -78,21 +61,20 @@
 													</tr>
 												</thead>
 												<?php
-													$ret=mysqli_query($con,"select *from  tblartist");
-													$cnt=1;
-													while ($row=mysqli_fetch_array($ret)) {
-													?>
-								  
+													$qArtist=mysqli_query($con,"SELECT * FROM  tblartist");
+													$count=1;
+													while ($data=mysqli_fetch_array($qArtist)) {
+													?>								  
 													<tr>
-														  <td><?php echo $cnt;?></td>
-														  <td><?php  echo $row['Name'];?></td>
-														  <td><?php  echo $row['Email'];?></td> 
-														  <td><?php  echo $row['MobileNumber'];?></td>
-														  <td><?php  echo $row['CreationDate'];?></td>
-														  <td><a href="editArtist.php?editid=<?php echo $row['ID'];?>" class="btn btn-success">Edit</a> || <a href="manageArtist.php?delid=<?php echo $row['ID'];?>" class="btn btn-danger">Delete</a></td>
+														  <td><?php echo $count;?></td>
+														  <td><?php  echo $data['Name'];?></td>
+														  <td><?php  echo $data['Email'];?></td> 
+														  <td><?php  echo $data['MobileNumber'];?></td>
+														  <td><?php  echo $data['CreationDate'];?></td>
+														  <td><a href="editArtist.php?editid=<?php echo $data['ID'];?>" class="btn btn-success">Edit</a> || <a href="handleArtist.php?delid=<?php echo $data['ID'];?>" class="btn btn-danger">Delete</a></td>
 													</tr>
 													<?php 
-														$cnt=$cnt+1;
+														$count=$count+1;
 													}
 													?>
 										</table>
@@ -100,23 +82,17 @@
 						  </div>
 					</div>
 				   
-					<!-- page end-->
+				
 				  </section>
 			</section>
-    <!--main content end-->
+  
     <?php include_once('includes/footer.php');?>
-  </section>
-  <!-- container section end -->
-  <!-- javascripts -->
+  </section> 
   <script src="js/jquery.js"></script>
   <script src="js/bootstrap.min.js"></script>
-  <!-- nicescroll -->
   <script src="js/jquery.scrollTo.min.js"></script>
   <script src="js/jquery.nicescroll.js" type="text/javascript"></script>
-  <!--custome script for all page-->
   <script src="js/scripts.js"></script>
-
-
 </body>
 
 </html>

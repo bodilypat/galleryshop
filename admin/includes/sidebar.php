f8ee62513ddbacd6b450351ba9ed4b9136f0fa87
bodilypat <139784676+bodilypat@users.noreply.github.com>
2023-08-24 09:05:04 -0700

    Create sidebar.php

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,90 @@
+<aside>
+      <div id="sidebar" class="nav-collapse ">
+        <!-- sidebar menu start-->
+        <ul class="sidebar-menu">
+          <li class="active">
+            <a class="" href="dashboard.php">
+                          <i class="icon_house_alt"></i>
+                          <span>Dashboard</span>
+                      </a>
+          </li>
+   
+          <li class="sub-menu">
+            <a href="javascript:;" class="">
+                          <i class="icon_desktop"></i>
+                          <span>Artist</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+            <ul class="sub">
+              <li><a class="" href="addArtist.php">Add Artist</a></li>
+              <li><a class="" href="handleArtist.php">Manage Artist</a></li>
+             
+            </ul>
+          </li>
+                <li class="sub-menu">
+            <a href="javascript:;" class="">
+                          <i class="icon_document_alt"></i>
+                          <span>Art Type</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+            <ul class="sub">
+              <li><a class="" href="addType.php">Add Type</a></li>
+              <li><a class="" href="handleArttype.php">Manage Type</a></li>
+            </ul>
+          </li>
+          <li class="sub-menu">
+            <a href="javascript:;" class="">
+                          <i class="icon_document_alt"></i>
+                          <span>Medium of Art</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+            <ul class="sub">
+              <li><a class="" href="addMedium.php">Add Medium</a></li>
+              <li><a class="" href="handleArtMedium.php">Manage Medium</a></li>
+            </ul>
+          </li>
+ <li class="sub-menu">
+            <a href="javascript:;" class="">
+                          <i class="icon_document_alt"></i>
+                          <span>Art Products</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+            <ul class="sub">
+              <li><a class="" href="addProduct.php">Add Product</a></li>
+              <li><a class="" href="handleProduct.php">Manage Product</a></li>
+            </ul>
+          </li>
+          <li class="sub-menu">
+            <a href="javascript:;" class="">
+                          <i class="icon_table"></i>
+                          <span>Inquiry</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+            <ul class="sub">
+              <li><a class="" href="unanswerInquiry.php">Unanswer Inquiry</a></li>
+              <li><a class="" href="answerInquiry.php">Answered Inquiry</a></li>
+            </ul>
+          </li>
+<li>
+            <a class="" href="search.php">
+                          <i class="icon_genius"></i>
+                          <span>Search Inquiry</span>
+                      </a>
+          </li>
+          <li class="sub-menu">
+            <a href="javascript:;" class="">
+                          <i class="icon_documents_alt"></i>
+                          <span>Pages</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+            <ul class="sub">
+              <li><a class="" href="aboutus.php"><span>About Us</span></a></li>
+              <li><a class="" href="contactus.php"><span>Contact Us</span></a></li>
+              
+            </ul>
+          </li>
+
+        </ul>
+        <!-- sidebar menu end-->
+      </div>
+    </aside>

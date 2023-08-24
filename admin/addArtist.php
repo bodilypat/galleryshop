d5fffd80fab6877bf51863802a41d7c07a6700a9
bodilypat <139784676+bodilypat@users.noreply.github.com>
2023-08-23 21:39:52 -0700

    Update addArtist.php

--- admin/addArtist.php
+++ admin/addArtist.php
@@ -51,20 +51,12 @@ if (strlen($_SESSION['agmsaid']==0)) {
 
   <!-- Bootstrap CSS -->
   <link href="css/bootstrap.min.css" rel="stylesheet">
-  <!-- bootstrap theme -->
   <link href="css/bootstrap-theme.css" rel="stylesheet">
-  <!--external css-->
-  <!-- font icon -->
   <link href="css/elegant-icons-style.css" rel="stylesheet" />
   <link href="css/font-awesome.min.css" rel="stylesheet" />
   <link href="css/daterangepicker.css" rel="stylesheet" />
   <link href="css/bootstrap-datepicker.css" rel="stylesheet" />
   <link href="css/bootstrap-colorpicker.css" rel="stylesheet" />
-  <!-- date picker -->
-
-  <!-- color picker -->
-
-  <!-- Custom styles -->
   <link href="css/style.css" rel="stylesheet">
   <link href="css/style-responsive.css" rel="stylesheet" />
 
@@ -74,15 +66,8 @@ if (strlen($_SESSION['agmsaid']==0)) {
 
   <!-- container section start -->
   <section id="container" class="">
-    <!--header start-->
     <?php include_once('includes/header.php');?>
-    <!--header end-->
-
-    <!--sidebar start-->
-   <?php include_once('includes/sidebar.php');?>
-    <!--sidebar end-->
-
-    <!--main content start-->
+    <?php include_once('includes/sidebar.php');?>
     <section id="main-content">
       <section class="wrapper">
         <div class="row">
@@ -102,8 +87,7 @@ if (strlen($_SESSION['agmsaid']==0)) {
              Add Artist Detail
               </header>
               <div class="panel-body">
-                <form class="form-horizontal " method="post" action="" enctype="multipart/form-data">
-                  
+                <form class="form-horizontal " method="post" action="" enctype="multipart/form-data"> 
                   <div class="form-group">
                     <label class="col-sm-2 control-label">Name</label>
                     <div class="col-sm-10">
@@ -142,33 +126,20 @@ if (strlen($_SESSION['agmsaid']==0)) {
             
           </div>
         </div>
-        <!-- Basic Forms & Horizontal Forms--
         <!-- page end-->
       </section>
     </section>
  <?php include_once('includes/footer.php');?>
   </section>
-  <!-- container section end -->
-  <!-- javascripts -->
+
   <script src="js/jquery.js"></script>
   <script src="js/bootstrap.min.js"></script>
-  <!-- nice scroll -->
   <script src="js/jquery.scrollTo.min.js"></script>
   <script src="js/jquery.nicescroll.js" type="text/javascript"></script>
-
-  <!-- jquery ui -->
   <script src="js/jquery-ui-1.9.2.custom.min.js"></script>
-
-  <!--custom checkbox & radio-->
   <script type="text/javascript" src="js/ga.js"></script>
-  <!--custom switch-->
   <script src="js/bootstrap-switch.js"></script>
-  <!--custom tagsinput-->
   <script src="js/jquery.tagsinput.js"></script>
-
-  <!-- colorpicker -->
-
-  <!-- bootstrap-wysiwyg -->
   <script src="js/jquery.hotkeys.js"></script>
   <script src="js/bootstrap-wysiwyg.js"></script>
   <script src="js/bootstrap-wysiwyg-custom.js"></script>
@@ -176,13 +147,9 @@ if (strlen($_SESSION['agmsaid']==0)) {
   <script src="js/bootstrap-colorpicker.js"></script>
   <script src="js/daterangepicker.js"></script>
   <script src="js/bootstrap-datepicker.js"></script>
-  <!-- ck editor -->
   <script src="js/ckeditor.js" type="text/javascript" ></script>
-  <!-- custom form component script for this page-->
   <script src="js/form-component.js"></script>
-  <!-- custome script for all page -->
   <script src="js/scripts.js"></script>
-
 </body>
 
 </html>
